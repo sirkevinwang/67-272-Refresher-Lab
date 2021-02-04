b0ba677a81776930da90f1fbad3ba22470b939de
Kevin Wang <kevin.wang11@outlook.com>
2021-02-04 15:33:31 -0500

    all model tests passed

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,25 @@
 class Chore < ApplicationRecord
+  # Relations
   belongs_to :child
   belongs_to :task
+
+  # Scopes 
+  scope :by_task, -> { joins(:task).order("tasks.name") }
+  scope :pending, -> { where(completed: false) }
+  scope :done, -> { where(completed: true) }
+  scope :upcoming, -> { where('due_on >= ?', Date.current) }
+  scope :past, -> { where('due_on < ?', Date.current) }
+  scope :chronological, -> { order('due_on, completed') }
+
+  # Validators
+  validates_date :due_on
+  
+  # Methods
+  def status
+    if self.completed
+      return "Completed"
+    end
+    "Pending"
+  end
+  
 end
