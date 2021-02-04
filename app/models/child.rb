d7c19adf2d5798fc38b10495428df15645035be6
Kevin Wang <kevin.wang11@outlook.com>
2021-02-04 11:00:22 -0500

    child model all passed

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,8 +1,17 @@
 class Child < ApplicationRecord
   # Relations
   has_many :chores
-  has_many :tasks, through: :chores
+  has_many :tasks, through: :chores 
+  
+  # Scopes
+  scope :alphabetical, -> { order('last_name, first_name') }
+  scope :active, -> { where(active: true) }
 
   # Validations
   validates_presence_of :first_name, :last_name
+
+  # Methods
+  def name
+    self.first_name + " " + self.last_name
+  end
 end
